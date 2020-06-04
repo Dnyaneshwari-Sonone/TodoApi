18e5ee45d6fcc8c4a541c13f92b308f39144d13d
Dnyaneshwari Sonone <t-dnsono@microsoft.com>
2020-06-04 11:53:34 +0530

    Add project files.

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        public string Name { get; set; }
+
+        [System.ComponentModel.DefaultValue(false)]
+        public bool IsComplete { get; set; }
+
+        public string Secret { get; set; }
+    }
+}
